0303c89b4c3cb191ecf6ab153d00db0c5fe57fc5
David Burke <david.burke@fundingcircle.com>
2017-08-06 11:28:08 -0700

    Added empty details screen

---
+++ TastingNotes/TastingSessionDetailsVC.swift
@@ -0,0 +1,16 @@
+//
+//  TastingSessionDetailsVC.swift
+//  TastingNotes
+//
+//  Created by David Burke on 8/6/17.
+//  Copyright © 2017 amberfire. All rights reserved.
+//
+
+import UIKit
+
+class TastingSessionDetailsVC: UIViewController {
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
